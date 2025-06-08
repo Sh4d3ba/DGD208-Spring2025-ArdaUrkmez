f74b673ec379698d887b3e25fb97d9b2503fde74
Sh4d3ba <153110835+Sh4d3ba@users.noreply.github.com>
2025-06-08 09:01:26 +0300

    commit

--- GameProg/GameProg/obj/Debug/net9.0/GameProg.AssemblyInfo.cs
+++ GameProg/GameProg/obj/Debug/net9.0/GameProg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameProg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c98e5286263807cf7af824ad6cf384e4db79450d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29d6b90f1d71eae1f59ce695bd1b6df6a12944c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameProg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameProg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
